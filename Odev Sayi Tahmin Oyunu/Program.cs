09f447af5db07b55870455f746d173c7c09e160a
Enes <ardaayaz2019@gmail.com>
2024-01-03 01:18:51 +0300

    update

--- Odev Sayi Tahmin Oyunu/Program.cs
+++ Odev Sayi Tahmin Oyunu/Program.cs
@@ -8,80 +8,50 @@
             int can = 100;
             Console.WriteLine("seviye seciniz:\n1-kolay\n2-orta\n3-zor");
             int zorluk = int.Parse(Console.ReadLine());
-            int value = 0;
+            int value = 0; int eksilt = 0;
+            
             switch (zorluk)
             {
                 case 1:
                     value = 10;
+                    eksilt = 10;
+                    
                     break;
                 case 2:
                     value = 55;
+                    eksilt = 50;
                     break;
                 case 3:
                     value = 100;
+                    eksilt=100;
                     break;
                 default:
                     break;
             }
-
+            
             Random rnd = new Random();
             int sayı = rnd.Next(value);
             do
             {
                 Console.WriteLine("sayı tahmin et");
                 tahmin = int.Parse(Console.ReadLine());
-                if (zorluk == 1)
-                {
-                    if (tahmin != sayı)
-                    {
-                        can -= 20;
-                        if (can == 0)
-                        {
-                            Console.WriteLine("CANIN BİTTİ...");
-                            break;
-                        }
-                    }
-                }
 
-                else if (zorluk == 2)
+                if (tahmin == sayı)
                 {
-                    if (tahmin != sayı)
-                    {
-                        can -= 50;
-                        if (can == 0)
-                        {
-                            Console.WriteLine("CANIN BİTTİ...");
-                            break;
-                        }
-                    }
-                }
-                else if (zorluk == 3)
-                {
-                    if (tahmin != sayı)
-                    {
-                        can -= 100;
-                        if (can == 0)
-                        {
-                            Console.WriteLine("CANIN BİTTİ...");
-                            break;
-                        }
-                    }
-
+                    Console.WriteLine("DOGRU TAHMİN...");
                 }
                 if (tahmin < sayı)
-                {
-                    Console.WriteLine("Büyük bir sayı tahmin ediniz");
+                {      
+                    can -= eksilt;
                 }
                 else if (tahmin > sayı)
-                {
-                    Console.WriteLine("Küçük bir sayı tahmin ediniz");
+                { 
+                    can -= eksilt;
                 }
+                if (can == 0) { break; }
             }
             while (tahmin != sayı);
-            if (tahmin == sayı)
-            {
-                Console.WriteLine("dogru tahmin...");
-            }
+
         }
     }
 }
